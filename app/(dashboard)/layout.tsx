e5189a18caaa7ddabfa3f778dea6a69eb7bbf45b
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2026-06-29 08:20:48 +0600

    update

--- app/(dashboard)/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen">
-      {/* TODO: add sidebar / topbar */}
-      <main className="p-6">{children}</main>
-    </div>
-  );
-}
